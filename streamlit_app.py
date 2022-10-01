4dc70ee7b6d4c9a5601f7df17927e37b470644ce
lokeshkonda <97781567+lokeshkonda@users.noreply.github.com>
2022-10-01 20:10:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard_Boiled Free_Range Egg')
-streamlit.text('🥑🍞Hard_Boiled Free_Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
