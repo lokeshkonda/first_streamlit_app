2716df96ab9142a6c60f3aa867c6e539225019bc
lokeshkonda <97781567+lokeshkonda@users.noreply.github.com>
2022-10-03 21:04:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+#from urllib.error import URLError
 
 streamlit.header('My Moms New Healthy Dinner')
 streamlit.header('Breakfast Favorites')
@@ -47,7 +47,7 @@ try:
 
 #streamlit.dataframe(fruityvice_normalized)
 
-#streamlit.stop()
+streamlit.stop()
 
 
 
